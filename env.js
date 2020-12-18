1a20965343ed9fe5d942ef10fcdaca909134e642
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-12-18 13:18:52 -0500

    Env Setup Script

--- env.js
+++ env.js
@@ -1,11 +1,28 @@
-const prompts = require('prompts');
- 
+const prompts = require("prompts");
+const fs = require("fs");
 (async () => {
-  const response = await prompts({
-    type: 'text',
-    name: 'meaning',
-    message: 'What is the meaning of life?'
-  });
- 
-  console.log(response.meaning);
+  const response = await prompts([
+    {
+      type: "text",
+      name: "url",
+      message: "What is your MongoDB URL?",
+    },
+    {
+      type: "text",
+      name: "port",
+      message: "What is your port?",
+      initial: "8080",
+    }
+  ]);
+
+  fs.writeFile(
+    ".env",
+    `DB_URL=${response.url}
+LISTEN_PORT=${response.port}`,
+    "utf8",
+    function () {
+      console.log("Your settings have been written to .env!");
+      console.log("Run npm run serve to serve your instance.");
+    }
+  );
 })();
