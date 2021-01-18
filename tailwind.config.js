2c7ef01a71fa3bd93b43489ca1dbadaf0e9ddab6
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-18 13:52:30 -0500

    Fix glitch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
+
   purge: [],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },    
     extend: {
       spacing: {
         '28': '7rem',
